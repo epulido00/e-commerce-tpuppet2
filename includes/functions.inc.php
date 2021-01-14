14dd58898bfdc3b0b0e1de0ff7963ab38a4f8aa3
Angela-tb <angela_michel_tb@hotmail.com>
2021-01-14 00:07:12 -0600

    Login done

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -81,5 +81,38 @@ function createUser($conn, $name,$email,$user,$pwd){
     exit();
   }
 
+  function emptyInputLogin($user,$pwd){
+    $result=true;
+    if( empty($user) || empty($pwd)){
+        $result=true;
+    }else{
+        $result=false;
+    }
+    return $result;
+    }
 
+  function  loginUser($conn, $user, $pwd ){
+      $userExist = userExists($conn,$user,$user);
+
+      if($userExist=== false){
+        header("location: ../login.php?error=wronglogin");
+        exit(); 
+      }
+
+      $pwdHashed = $userExist["usersPwd"];
+      $checkPwd = password_verify($pwd, $pwdHashed);
+
+      if($checkPwd === false) {
+        header("location: ../login.php?error=wronglogin");
+        exit(); 
+      }
+      else if($checkPwd === true){
+          session_start();
+        $_SESSIOM["userid"] = $userExist["usersID"];
+        $_SESSION["useruid"] = $userExist["usersUid"];
+        header("location: ../index.php");
+        exit(); 
+
+      }
+  }
 
--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,4 +1,24 @@
 <?php
+if(isset($_POST["submit"])){
 
+$user = $_POST["user"];
+$pwd = $_POST["pwd"];
 
-?>
+require_once 'dbh.inc.php';
+require_once 'functions.inc.php';
+
+if(emptyInputLogin($user,$pwd) !== false){
+    header("location: ../login.php?error=emptyinput");
+    exit();
+}
+
+loginUser($conn, $user, $pwd );
+}
+
+else {
+
+header("location: ../login.php");
+exit();
+
+}
+
