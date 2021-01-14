659235d4f617eaba2b91a78e168f408ac883cb58
jessycavazos22 <jessycavazos2207@gmail.com>
2021-01-13 20:23:15 -0600

    ahora si el bueno

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,11 +5,13 @@ $dBUsername = "root";
 $dBPassword = "";
 $dBName = "phpproject";
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName, "3307");
 
 if(!$conn){
 
     die("Connection failed: " . mysqli_connect_error());
 }
+
+mysqli_query($conn,"insert into users values (1,'Jessica Cavazos','jessy@gmail.com','jessy123','hola123')");
 
 ?>
