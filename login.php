9d228861a3636ef8805a72cb21505426e6b7e027
jessycavazos22 <jessycavazos2207@gmail.com>
2021-01-13 19:06:07 -0600

    Sign up / Login html

--- login.php
+++ login.php
@@ -17,6 +17,7 @@
 	<!--Custom styles-->
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
@@ -30,27 +31,29 @@
 				</div>
 			</div>
 			<div class="card-body">
-				<form>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
+			<br><br>
+			  <section class="login-form">
+                    <form action="login.inc.php" method="post">
+						<div class="input-group form-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text" class="form-control" placeholder="Usuario">	
 						</div>
-						<input type="text" class="form-control" placeholder="username">
-						
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						<div class="input-group form-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" class="form-control" placeholder="Contraseña">
 						</div>
-						<input type="password" class="form-control" placeholder="password">
-					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
+						<div class="row align-items-center remember">
+							<input type="checkbox">Remember Me
+						</div>
+						<div class="form-group">
+							<input type="submit" value="Login" class="btn float-right login_btn">
+						</div>
 				</form>
+			  </section>
 			</div>
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
